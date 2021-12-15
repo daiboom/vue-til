ad9181a517985285f32e743b52703fec063d768b
ChoeDaeBeom <chleoqja1760@gmail.com>
2021-12-16 08:57:55 +0900

    add

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,23 +1,52 @@
 <template>
-	<form action="@">
+	<form @submit.prevent="submitForm">
 		<div>
 			<label for="username">id: </label>
-			<input id="username" type="text" />
+			<input id="username" type="text" v-model="username" />
 		</div>
 		<div>
 			<label for="password">pw: </label>
-			<input id="password" type="password" />
+			<input id="password" type="password" v-model="password" />
 		</div>
 		<div>
 			<label for="nickname">nickname: </label>
-			<input id="nickname" type="text" />
+			<input id="nickname" type="text" v-model="nickname" />
 		</div>
 		<button type="submit">login</button>
+		<p>{{ logMessage }}</p>
 	</form>
 </template>
 
 <script>
-export default {}
+import { registerUser } from '@/api/index'
+export default {
+	data() {
+		return {
+			username: '',
+			password: '',
+			nickname: '',
+			logMessage: '',
+		}
+	},
+	methods: {
+		async submitForm() {
+			const userData = {
+				username: this.username,
+				password: this.password,
+				nickname: this.nickname,
+			}
+			const { data } = await registerUser(userData)
+			console.log(data)
+			this.logMessage = `${data.username} 가입되었습니다.`
+			this.initForm()
+		},
+		initForm() {
+			this.username = ''
+			this.password = ''
+			this.nickname = ''
+		},
+	},
+}
 </script>
 
 <style></style>
