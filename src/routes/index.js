b9fe60f89192ad4d934d976d3e013d7de4a77df8
ChoeDaeBeom <chleoqja1760@gmail.com>
2021-12-26 20:05:36 +0900

    add

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,7 +20,10 @@ export default new VueRouter({
 			path: '/signup',
 			component: () => import('@/views/SignupPage.vue'),
 		},
-		{ path: '/main', component: () => import('@/views/MainPage.vue') },
+		{
+			path: '/main',
+			component: () => import('@/views/MainPage.vue'),
+		},
 		{
 			path: '*',
 			component: () => import('@/views/NotFoundPage.vue'),
